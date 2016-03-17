53f081f8797cb4757645526aa6768b845782e3d7
wgrzesiak147 <wgrzesiak147@gmail.com>
2016-03-17 13:27:25 +0100

    GetById test added

---
+++ DatabaseSimulator/DatabaseSimulator.Tests/Tests/GetByIdTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseSimulator.Tests.Tests
+{
+    public class GetByIdTest : Test
+    {
+        private int _id;
+        public GetByIdTest(RichTextBox output, IDatabaseManager database,int id) : base(output, database)
+        {
+            _id = id;
+        }
+
+        protected override string TestName { get; set; } = "GetById";
+        protected override void TestLogic()
+        {
+            Database.GetProductByID(_id);
+            Output.AppendText(String.Format("Product with id: {0} received",_id));
+        }
+    }
+}
